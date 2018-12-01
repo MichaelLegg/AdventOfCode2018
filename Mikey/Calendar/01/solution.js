e0015c95dfab61798badd6d61a4352b2a70195b6
Michael Legg <michael.legg@outlook.com>
2018-12-01 19:45:46 +0000

    setup project structure

---
+++ Mikey/Calendar/01/solution.js
@@ -0,0 +1,5 @@
+module.exports = { solve }
+
+function solve (input, part) {
+  return 'aseasd'
+}
--- Mikey/index.js
+++ Mikey/index.js
@@ -1,1 +1,28 @@
-// Index file that runs the calendar day specified
+// Index file that runs the calendar day specifiedfor(let arg of args)
+const chalk = require('chalk')
+const fs = require('fs')
+
+// get the argument from the run command (first in array is node exe, second is index.js filename)
+let arg = process.argv.slice(2)
+
+const path = './Calendar/' + arg
+
+if (!fs.existsSync(path)) {
+  console.log(chalk.bold.red('Folder for that day does not exist'))
+  process.exit()
+}
+
+console.log(chalk.blue('Welcome to the 2018 Advent of Code!'))
+console.log(chalk.green('Running day'), chalk.yellow(arg) + '.')
+
+const solution = require(path + '/solution.js')
+
+const input = fs.readFileSync('Calendar/' + arg + '/input.txt').toString()
+
+console.log(chalk.white.bold.underline('\n______Part 1______'))
+console.log(chalk.cyan('Input  is: ' + input))
+console.log(chalk.magenta('Solution is: ' + solution.solve(input, 1)))
+
+console.log(chalk.white.bold.underline('\n______Part 2______'))
+console.log(chalk.cyan('Input  is: ' + input))
+console.log(chalk.magenta('Solution is: ' + solution.solve(input, 2)))
